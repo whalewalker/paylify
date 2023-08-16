65ef6a92a079579e36e67c0ea5611a1c3d46b94a
whalewalker <mongodbms@gmail.com>
2023-08-16 04:25:43 +0100

    project setup

---
+++ src/main/java/com/paylify/PaylifyApplication.java
@@ -0,0 +1,13 @@
+package com.paylify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaylifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaylifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paylify/PaylifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paylify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaylifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
